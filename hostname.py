5f22dd9bfff9eee6e62a1b24656e9ece1282dc94
jose javier garcia aranda <jjaranda13@hotmail.com>
2022-08-16 14:22:53 +0200

    version inicial

---
+++ hostname.py
@@ -0,0 +1,71 @@
+from math import log10, sqrt
+
+import os
+from pathlib import Path
+import time
+#para instalar el modulo easygui simplemente:
+#pip3 install easygui
+# o bien py -m pip install easygui
+import easygui 
+import lock
+
+import socket
+import fnmatch
+# variables globales
+# ------------------
+props_dict={} 
+DEBUG_MODE=True
+
+def init(props):
+    global props_dict
+    print("Python: Enter in init")
+    
+    #props es un diccionario
+    props_dict= props
+
+    return 0
+
+
+def executeChallenge():
+    print("Starting execute")
+    
+    
+    #mecanismo de lock BEGIN, para garantizar una sola interaccion con user a la vez
+    #-----------------------
+    #no lo necesito porque no es intrusivo
+    #lock.lockIN("hostname")
+    
+    #pedimos password
+    hostname = socket.gethostname()
+
+    #mecanismo lock out
+    #lock.lockOUT("hostname")
+    
+    #ahora comparamos con la correcta
+    hostname_start=props_dict["hostname_start"]
+    lenstart=len (hostname_start)
+    if fnmatch.fnmatch(hostname,hostname_start+"*"):
+        print ("el pc es corporativo. Hostname=", hostname)
+    cad= hostname[:lenstart]
+    print (cad)
+    
+    
+    
+    key = bytes(cad,'utf-8')
+    key_size = len(key)
+
+    result =(key, key_size)
+    print ("result:",result)
+    return result
+
+
+
+    
+
+
+if __name__ == "__main__":
+    #mode "parental" o "normal"
+    midict={"hostname_start": "N-"} #"clave": "clavesecreta", "mode":"parental"}
+    init(midict)
+    executeChallenge()
+
